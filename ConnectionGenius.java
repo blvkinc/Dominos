1bcbfd87ef394f7af7ae0d62f4e601b071eb6da2
Rndmun <91401175+Rndmun@users.noreply.github.com>
2023-11-14 05:46:44 -0800

    inital codebase

--- ConnectionGenius.java
+++ ConnectionGenius.java
@@ -1,35 +1,35 @@
-     1	package base;
-     2	import java.net.InetAddress;
-     3	/**
-     4	 * @author Kevan Buckley, maintained by __student
-     5	 * @version 2.0, 2014
-     6	 */
-     7	
-     8	public class ConnectionGenius {
-     9	
-    10	  InetAddress ipa;
-    11	  
-    12	  public ConnectionGenius(InetAddress ipa) {
-    13	    this.ipa = ipa;
-    14	  }
-    15	
-    16	  public void fireUpGame() {
-    17	    downloadWebVersion();
-    18	    connectToWebService();
-    19	    awayWeGo();
-    20	  }
-    21	  
-    22	  public void downloadWebVersion(){
-    23	    System.out.println("Getting specialised web version.");
-    24	    System.out.println("Wait a couple of moments");  
-    25	  }
-    26	  
-    27	  public void connectToWebService() {
-    28	    System.out.println("Connecting");
-    29	  }
-    30	  
-    31	  public void awayWeGo(){
-    32	    System.out.println("Ready to play");
-    33	  }
-    34	
-    35	}
+package base;
+import java.net.InetAddress;
+
+/**
+ * @author Kevan Buckley, maintained by __student
+ * @version 2.0, 2014
+ */
+
+public class ConnectionGenius {
+
+  InetAddress ipa;
+
+  public ConnectionGenius(InetAddress ipa) {
+    this.ipa = ipa;
+  }
+
+  public void fireUpGame() {
+    downloadWebVersion();
+    connectToWebService();
+    awayWeGo();
+  }
+
+  public void downloadWebVersion(){
+    System.out.println("Getting specialised web version.");
+    System.out.println("Wait a couple of moments");  
+  }
+
+  public void connectToWebService() {
+    System.out.println("Connecting");
+  }
+
+  public void awayWeGo(){
+    System.out.println("Ready to play");
+  }
+}
--- _Q.java
+++ _Q.java
@@ -1,14 +1,15 @@
-     1	package base;
-     2	public class _Q {
-     3	  static transient String[] stuff = {
-     4	    "Progress comes from the intelligent use of experience.","Elbert Hubbard","",
-     5	    "No amount of experimentation can ever prove me right; a single experiment can prove me wrong.","Albert Einstein","",
-     6	    "To be trusted is a greater complement than to be loved.","George MacDonald","",
-     7	    "Everything has beauty, but not everyone sees it.","Confucius","",
-     8	    "Pretty much all the honest truth-telling there is in the world is done by children.","Oliver Wendell Holmes","",
-     9	    "A lie cannot live.","Martin Luther King Jr.","",
-    10	    "There are a lot of lies going around...and half of them are true.","Winston Churchill","",
-    11	    "Everyone is a moon and has a dark side which he never shows to anybody.","Mark Twain","",
-    12	    "Lies are usually caused by an undue fear of men.","Nachman of Bratslav","",
-    13	    "If we knew what it was we were doing, it would not be called research, would it?","Albert Einstein",""};
-    14	}
+package base;
+
+public class _Q {
+  static transient String[] stuff = {
+    "Progress comes from the intelligent use of experience.","Elbert Hubbard","",
+    "No amount of experimentation can ever prove me right; a single experiment can prove me wrong.","Albert Einstein","",
+    "To be trusted is a greater complement than to be loved.","George MacDonald","",
+    "Everything has beauty, but not everyone sees it.","Confucius","",
+    "Pretty much all the honest truth-telling there is in the world is done by children.","Oliver Wendell Holmes","",
+    "A lie cannot live.","Martin Luther King Jr.","",
+    "There are a lot of lies going around...and half of them are true.","Winston Churchill","",
+    "Everyone is a moon and has a dark side which he never shows to anybody.","Mark Twain","",
+    "Lies are usually caused by an undue fear of men.","Nachman of Bratslav","",
+    "If we knew what it was we were doing, it would not be called research, would it?","Albert Einstein",""};
+}
